660f6c7981932c25819f28de88ef74671b45761f
Gabriel Zafra <77026475+gabdrz@users.noreply.github.com>
2022-07-14 08:22:07 -0400

    Graph Progress
    - It sticks to the panel but it need to be configured still

--- app.js
+++ app.js
@@ -247,3 +247,21 @@ let boxTl = gsap.timeline()
 masterTl.play()
 
 // Page 3
+const N = 13;
+    const gData = {
+      nodes: [...Array(N).keys()].map(i => ({ id: i })),
+      links: [...Array(N).keys()]
+        .filter(id => id)
+        .map(id => ({
+          source: id,
+          target: Math.round(Math.random() * (id-1))
+        }))
+    };
+
+    const Graph = ForceGraph()
+      (document.getElementById('graph'))
+        .enableZoomInteraction(false)
+        .enablePanInteraction(false)
+        .linkDirectionalParticles(100)
+        .graphData(gData)
+        .nodeRelSize([20]);
